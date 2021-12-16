6eef54a2f99c24f2e5e7016f070991d4fe686f96
961026bm <olubunmiosunde.student@careerdevs.com>
2021-12-16 18:20:08 -0500

    solution to convert mins to secs

--- getfirstelement.js
+++ getfirstelement.js
@@ -2,30 +2,3 @@ function getFirstValue(arr) {
 	return arr[0];
 
 }
-/* in java object are stored in class called (public class )
-a method is a function stored in an object
-
-public class Challenge {
-	public static int getFirstValueInt([] arr) {
-		return arr[0]
-	}
-}
-
-public class Person {
-	public static myfunc () {
-
-		int firstname = Challenge.getFirstValue(new int[]{1, 2, 3})
-	}
-}
-
-||
-	public class Challenge {
-			public static int getFirstValueInt([] arr) {
-				int firstnum = arr[0]
-				System.out.printint(firstNum) => is how to console log in JAVA
-				return firsnum
-			}
-		}
-		// diff b/w static n non-static
-		static method can be called from a class without creating an active method 
- */
